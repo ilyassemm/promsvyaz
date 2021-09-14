9166e273adc271ff16a613a6bfd1240c27dd31f1
ilyassemm <semenov_ilya_01@mail.ru>
2021-09-14 22:09:40 +0300

    добавлен стиль при наведении

--- psb-frontend/src/App.vue
+++ psb-frontend/src/App.vue
@@ -57,13 +57,38 @@ img {
 .nav-link {
 
    padding: 16px 0;
-   display: inline-block;
+   text-decoration:none;
    cursor: pointer;
-   color: #fff;
+   color: white !important;
    font-size: 13px;
    line-height: 18px;
    font-weight: 600;
    position: relative;
    font-family: 'Arial', sans-serif;
+   
 }
+
+.nav-link {
+text-decoration:none; /*Убираем стандартное подчёркивание ссылки если оно ещё не убрано*/
+display: inline-block; /*делаем чтобы наша ссылка из строчного элемента превратилась в строчно-блочный 
+line-height: 1; /*Задаём высоту строки (можно в пикселях)*/
+color:#2F73B6;/*Задаём цвет ссылки*/
+}
+.nav-link:after {
+display: block; /*превращаем его в блочный элемент*/
+content: ""; /*контента в данном блоке не будет поэтому в кавычках ничего не ставим*/
+height: 3px; /*задаём высоту линии*/
+width: 0%; /*задаём начальную ширину элемента (линии)*/
+background-color: white; /*цвет фона элемента*/
+transition: width 0.4s ease-in-out; /*данное свойство отвечает за плавное изменение ширины. Здесь можно задать время анимации в секундах (в данном случае задано 0.4 секунды)*/
+}
+.nav-link:hover:after,
+.nav-link:focus:after {
+width: 100%;
+}
+.nav-link:hover{
+color:white;
+} 
+
+
 </style>

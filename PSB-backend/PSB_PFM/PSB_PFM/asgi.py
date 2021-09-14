832b4dc4e0578ec6f834085496ad6d896e6b7e2c
ilyassemm <semenov_ilya_01@mail.ru>
2021-09-14 19:56:39 +0300

    adding files

---
+++ PSB-backend/PSB_PFM/PSB_PFM/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PSB_PFM project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PSB_PFM.settings')
+
+application = get_asgi_application()
